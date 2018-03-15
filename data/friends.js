f8357c6faa0d60afdedfe404e3b6767eff9e40d6
paulaperoutka <paulaperoutka@gmail.com>
2018-03-15 13:35:47 -0400

    made into array and exported

--- data/friends.js
+++ data/friends.js
@@ -1,5 +1,5 @@
 // You should save your application's data inside of app/data/friends.js as an array of objects. Each of these objects should roughly follow the format below.
-[
+let friendsArray = [
 	{
 	  "name":"Ahmed",
 	  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
@@ -17,7 +17,8 @@
 	    ]
 	},
 	{
-		
+
 	}
 ]
 
+module.exports = friendsArray;
